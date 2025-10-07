fc2be746436fe60ecfb9fbf2e41c0aa59e52e55b
Kryu <kryu_maro_veracruz@dlsl.edu.ph>
2025-10-07 13:58:20 +0800

    vercel support

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Magical Boris-0.1.0";
+const contentToCache = [
+    "Build/VercelBoirs.loader.js",
+    "Build/VercelBoirs.framework.js",
+    "Build/VercelBoirs.data",
+    "Build/VercelBoirs.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
